412212e2d0e961e41c8c285b821aa0dd4aa9d9b8
hyr <2229795409@qq.com>
2023-06-08 17:10:12 +0800

    ll

--- vue_project/src/App.vue
+++ vue_project/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <!-- <validate></validate> -->
     <!-- <login></login> -->
-    <!-- <navigtion></navigtion> -->
+    <navigtion></navigtion>
     <!-- <firstAid></firstAid> -->
     <!-- <index></index> -->
     <personalSide></personalSide>
@@ -13,12 +13,12 @@
 import PersonalSide from './components/personal/personalSide'
 // import validate from './components/validate';
 // import login from './components/login';
-// import navigtion from './components/navigtion';
+import navigtion from './components/navigtion';
 // import firstAid from './components/firstAid';
 // import index from './components/index'
 export default {
   name: "App",
-  components: {PersonalSide },
+  components: {PersonalSide,navigtion },
 };
 
 </script>
--- vue_project/src/components/personal/personalSide.vue
+++ vue_project/src/components/personal/personalSide.vue
@@ -2,7 +2,6 @@
   <div>
     <el-row class="tac">
       <el-col :span="12">
-        <h5>自定义颜色</h5>
         <el-menu
           default-active="2"
           class="el-menu-vertical-demo"
@@ -12,27 +11,25 @@
           text-color="#fff"
           active-text-color="#ffd04b"
         >
-          <el-submenu index="1">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>导航一</span>
-            </template>
-            <el-menu-item-group>
-              <template slot="title">分组一</template>
-              <el-menu-item index="1-1">选项1</el-menu-item>
-              <el-menu-item index="1-2">选项2</el-menu-item>
-            </el-menu-item-group>
-            <el-menu-item-group title="分组2">
-              <el-menu-item index="1-3">选项3</el-menu-item>
-            </el-menu-item-group>
-            <el-submenu index="1-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="1-4-1">选项1</el-menu-item>
-            </el-submenu>
-          </el-submenu>
+        <div class="picture">
+          <div><img src="../../assets/tx.jpg" alt=""></div>
+          <div>用户名</div>
+        </div>
           <el-menu-item index="2">
             <i class="el-icon-menu"></i>
             <span slot="title">导航二</span>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <i class="el-icon-setting"></i>
+            <span slot="title">导航三</span>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <i class="el-icon-setting"></i>
+            <span slot="title">导航三</span>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <i class="el-icon-setting"></i>
+            <span slot="title">导航三</span>
           </el-menu-item>
           <el-menu-item index="3">
             <i class="el-icon-setting"></i>
@@ -59,4 +56,16 @@ export default {
 </script>
 
 <style scoped>
+.tac{
+  width: 400px;
+}
+.picture{
+  width: 400px;
+}
+.picture div img{
+  width: 80px;
+  height: 80px;
+  border-radius: 15px;
+}
+
 </style>
